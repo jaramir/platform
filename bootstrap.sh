4f471597a93c8626ac97e79f3740951bac197aeb
Francesco Gigli <francesco.gigli@eclinicalworks.com>
2023-06-05 21:12:48 +0200

    sync

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,16 +3,3 @@
 minikube start
 
 kubectl apply -k argocd
-
-while test -z $(kubectl get -n argocd pod -o name); do
-    echo -n "."
-    sleep 1
-done
-
-kubectl wait -n argocd --all pod --for condition=ready
-
-echo "---- ArgoCD Server ----"
-minikube service -n argocd argocd-server --url
-echo "Username: admin"
-echo -n "Password: "
-kubectl -n argocd get secret/argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d
