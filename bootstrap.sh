0f495c44a718ab6bac7450e44e24bcf1ff781666
Francesco Gigli <francesco.gigli@eclinicalworks.com>
2023-06-26 12:58:48 +0200

    dns

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-minikube start --addons=ingress
+minikube start --addons=ingress,ingress-dns
 
 kubectl apply -k argocd
 
 # The second time CRD will be present
 kubectl apply -k argocd
+
+echo "domain test
+nameserver `minikube ip`" > /etc/resolver/test
--- destroy.sh
+++ destroy.sh
@@ -1,3 +1,5 @@
 #!/bin/bash -xe
 
 minikube delete
+
+echo > /etc/resolver/test
